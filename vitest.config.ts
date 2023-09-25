8cf39cbec73f627500c8c3c024150ee88ed2e0c0
stLmpp <gui.stlmpp@gmail.com>
2023-09-24 22:53:40 -0300

    test: increase coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { vitestConfig } from '@assis-delivery/config';
+import { InlineConfig } from 'vitest';
 import { defineConfig, mergeConfig } from 'vitest/config';
 
-export default defineConfig(async (env) =>
-  mergeConfig(await vitestConfig(env), {}),
-);
+export default defineConfig(async (env) => {
+  const defaultConfig = await vitestConfig(env);
+  return mergeConfig(defaultConfig, {
+    test: {
+      coverage: {
+        exclude: [...defaultConfig.test.coverage.exclude, 'src/bin/bin.ts'],
+      },
+    } satisfies InlineConfig,
+  });
+});
