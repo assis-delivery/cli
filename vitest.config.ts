f56257d704eb3ddaf360b63e3dfccbbe9d9f0d7b
stLmpp <gui.stlmpp@gmail.com>
2023-09-24 23:23:48 -0300

    test: trying to solve the coverage problem

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,17 @@
 import { vitestConfig } from '@assis-delivery/config';
+import swc from 'unplugin-swc';
 import { InlineConfig } from 'vitest';
 import { defineConfig, mergeConfig } from 'vitest/config';
 
 export default defineConfig(async (env) => {
   const defaultConfig = await vitestConfig(env);
   return mergeConfig(defaultConfig, {
+    plugins: [
+      swc.vite({
+        module: { type: 'es6' },
+        sourceMaps: true,
+      }),
+    ],
     test: {
       coverage: {
         exclude: [...defaultConfig.test.coverage.exclude, 'src/bin/bin.ts'],
